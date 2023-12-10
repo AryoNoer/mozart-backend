ebc4be8f7ae44e2a66152157e71767c0b0e5e0e8
Aryo Noer <Aryojembaranmnoer@gmail.com>
2023-12-10 20:48:29 +0700

    3

--- app.py
+++ app.py
@@ -15,73 +15,93 @@ db = firestore.client()
 
 @app.route('/api/users', methods=['GET'])
 def get_data():
-    data = []
-    # Ambil data dari Firestore
-    collection_ref = db.collection('user')
-    docs = collection_ref.stream()
-    for doc in docs:
-        data.append(doc.to_dict())
-    return jsonify(data)
-
+    try:
+        data = []
+        # Ambil data dari Firestore
+        collection_ref = db.collection('user')
+        docs = collection_ref.stream()
+        for doc in docs:
+            data.append(doc.to_dict())
+        return jsonify(data)
+    except Exception as e:
+        app.logger.error(f"Error in GET request: {str(e)}")
+        return jsonify({"error": "Internal Server Error"}), 500
 
 # Route untuk menambahkan pengguna baru
+
+
 @app.route('/api/users', methods=['POST'])
 def add_data():
-    new_data = request.json.get("data")
+    try:
+        new_data = request.json.get("user")
 
-    if new_data is None:
-        return jsonify({"message": "Invalid data format"}), 400
+        if new_data is None:
+            return jsonify({"message": "Invalid data format"}), 400
 
-    # Tambahkan data ke Firestore
-    collection_ref = db.collection('user')
-    doc_ref, = collection_ref.add(new_data)
+        # Tambahkan data ke Firestore
+        collection_ref = db.collection('user')
+        doc_ref = collection_ref.add(new_data)
 
-    # Dapatkan data pengguna langsung setelah menambahkannya
-    user_data = doc_ref.get().to_dict()
+        # Dapatkan data pengguna langsung setelah menambahkannya
+        user_data = doc_ref.get().to_dict()
 
-    return jsonify({"message": "Data added successfully", "user": user_data}), 201
-
+        return jsonify({"message": "Data added successfully", "user": user_data}), 201
+    except Exception as e:
+        app.logger.error(f"Error in POST request: {str(e)}")
+        return jsonify({"error": "Internal Server Error"}), 500
 
 # Route untuk mendapatkan data pengguna berdasarkan ID
 
 
 @app.route('/api/users/<user_id>', methods=['GET'])
 def get_user(user_id):
-    user_ref = db.collection('user').document(user_id)
-    user_data = user_ref.get()
-    if user_data.exists:
-        return jsonify(user_data.to_dict())
-    else:
-        return jsonify({"message": "User not found"}), 404
+    try:
+        user_ref = db.collection('user').document(user_id)
+        user_data = user_ref.get()
+        if user_data.exists:
+            return jsonify(user_data.to_dict())
+        else:
+            return jsonify({"message": "User not found"}), 404
+    except Exception as e:
+        app.logger.error(f"Error in GET user request: {str(e)}")
+        return jsonify({"error": "Internal Server Error"}), 500
 
 # Route untuk mengupdate data pengguna berdasarkan ID
 
 
 @app.route('/api/users/<user_id>', methods=['PUT'])
 def update_user(user_id):
-    user_ref = db.collection('user').document(user_id)
-    user_data = user_ref.get()
+    try:
+        user_ref = db.collection('user').document(user_id)
+        user_data = user_ref.get()
 
-    if user_data.exists:
-        updated_data = request.json
-        user_ref.update(updated_data)
-        return jsonify({"message": "User updated successfully"}), 200
-    else:
-        return jsonify({"message": "User not found"}), 404
+        if user_data.exists:
+            updated_data = request.json
+            user_ref.update(updated_data)
+            return jsonify({"message": "User updated successfully"}), 200
+        else:
+            return jsonify({"message": "User not found"}), 404
+    except Exception as e:
+        app.logger.error(f"Error in PUT user request: {str(e)}")
+        return jsonify({"error": "Internal Server Error"}), 500
 
 # Route untuk menghapus data pengguna berdasarkan ID
 
 
 @app.route('/api/users/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    user_ref = db.collection('user').document(user_id)
-    user_data = user_ref.get()
+    try:
+        user_ref = db.collection('user').document(user_id)
+        user_data = user_ref.get()
 
-    if user_data.exists:
-        user_ref.delete()
-        return jsonify({"message": "User deleted successfully"}), 200
-    else:
-        return jsonify({"message": "User not found"}), 404
+        if user_data.exists:
+            user_ref.delete()
+            return jsonify({"message": "User deleted successfully"}), 200
+        else:
+            return jsonify({"message": "User not found"}), 404
+    except Exception as e:
+        app.logger.error(f"Error in DELETE user request: {str(e)}")
+        return jsonify({"error": "Internal Server Error"}), 500
 
 
 if __name__ == '__main__':
